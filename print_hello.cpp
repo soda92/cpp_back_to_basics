21e5896d8a1966efdbbff1ef059c788036be506b
SodaCris <18463922396@163.com>
2021-11-07 20:34:56 +0800

    add code

---
+++ print_hello.cpp
@@ -0,0 +1,3 @@
+#include <fmt/core.h>
+
+void print_hello() { fmt::print("{}\n", "Hello, World!"); }
---
+++ tu-1.cpp
@@ -0,0 +1,8 @@
+
+#include <fmt/core.h>
+int f(int);
+
+int main() {
+    fmt::print("{}\n", f(1));
+    return 0;
+}
