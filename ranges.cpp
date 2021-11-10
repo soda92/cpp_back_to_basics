77276e0c1cfffaa228f563fade1217f044a9b33a
SodaCris <18463922396@163.com>
2021-11-10 23:11:51 +0800

    refactor

--- ranges.cpp
+++ ranges.cpp
@@ -1,16 +1,11 @@
 #include <fmt/core.h>
 
 #include <range/v3/all.hpp>
-#include <vector>
+
+using namespace ranges;
 
 int main() {
-    std::vector<int> ints{0, 1, 2, 3, 4, 5};
-    auto even = [](int i) { return 0 == i % 2; };
-    auto square = [](int i) { return i * i; };
-    for (int i : ints | ranges::views::filter(even) |
-                     ranges::views::transform(square)) {
-        fmt::print("{} ", i);
-    }
+    for_each(views::iota(1, 11), [](auto v) { fmt::print("{} ", v * v); });
     fmt::print("\n");
     return 0;
 }
