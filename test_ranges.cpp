ed4cd61cb51910dc6a8c77912bf51948f1d9aae1
SodaCris <18463922396@163.com>
2021-11-10 22:50:48 +0800

    delete files

--- test_ranges.cpp
+++ test_ranges.cpp
@@ -1,17 +1,16 @@
 #include <fmt/core.h>
 
-#include <iostream>
-// #include <ranges>
+#include <range/v3/all.hpp>
 #include <vector>
 
 int main() {
     std::vector<int> ints{0, 1, 2, 3, 4, 5};
     auto even = [](int i) { return 0 == i % 2; };
     auto square = [](int i) { return i * i; };
-    // for (int i :
-    //      ints | std::view::filter(even) | std::view::transform(square)) {
-    //     fmt::print("{} ", i);
-    // }
+    for (int i : ints | ranges::views::filter(even) |
+                     ranges::views::transform(square)) {
+        fmt::print("{} ", i);
+    }
     fmt::print("\n");
     return 0;
 }
--- tu-1.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <fmt/core.h>
-int f(int);
-
-int main() {
-    fmt::print("{}\n", f(1));
-    return 0;
-}
