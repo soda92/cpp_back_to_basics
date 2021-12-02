87180c4d9954c1327fd3b23d95d54858d4b3fd9c
SodaCris <18463922396@163.com>
2021-12-02 09:05:23 +0800

    add string

---
+++ test_string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str =
+        "0 = xxx,"
+        "1 = yyy,"
+        "2 = zzz,"
+        "3 = aaa,"
+        "4 = bbb";
+    std::cout << str << "\n";
+    return 0;
+}
